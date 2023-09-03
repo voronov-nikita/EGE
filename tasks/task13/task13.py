695c028f57a3d831200b8cad74e00c577c4294e5
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-09-04 00:02:11 +0300

    change-in-13-task

--- tasks/task13/task13.py
+++ tasks/task13/task13.py
@@ -2,8 +2,19 @@ from ipaddress import *
 
 count:int = 0
 
-for ip in ip_network('192.168.32.160/255.255.255.240'):
+for ip in ip_network('145.92.137.88/255.255.240.0'):
     if bin(int(ip))[2:].count("1") % 2 == 0:
         count += 1
-        
+    print(ip)
 print(count)
+
+# Для задания с определением адреса сети
+ip = "145.92.137.88".split(".")
+mask = "255.255.240.0".split(".")
+
+answer = []
+
+for i in range(len(ip)):
+    answer.append(str(int(ip[i]) & int(mask[i])))
+    
+print('.'.join(answer))
